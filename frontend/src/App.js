ebaf41de1600d29527963b855f93471e1e13d60f
keshav <kdooleea@yahoo.ca>
2020-08-23 22:57:26 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+      <div className="first-layer">
+        <h1><span>Ama</span><span>'</span><span>Track</span></h1>
+        <a href="https://github.com/keshavDooleea?tab=repositories"
+          target="_blank"><i className="fab fa-github"></i></a>
+      </div>
+      <div className="sec-layer">
+        <small>Get notified when your favorite item is less expensive or avaliable!</small>
+      </div>
+      <div className="third-layer">
+        <div className="container">
+          <h1>fwofnwofn</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
