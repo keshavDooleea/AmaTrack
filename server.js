57da785e3872284565294cca32c6251219363aaf
keshav <kdooleea@yahoo.ca>
2020-08-27 19:46:15 -0400

    deployin to heroku

--- server.js
+++ server.js
@@ -13,4 +13,12 @@ app.get("/", (req, res) => {
 
 app.use("/product", require("./routes/product"));
 
+// deploy
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend/build", "index.html"));
+    });
+}
+
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
