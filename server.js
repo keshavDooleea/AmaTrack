8187a84706ed1e77569063b3c6228916452c5b0b
keshav <kdooleea@yahoo.ca>
2020-08-27 19:52:16 -0400

    tryin to deploy

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.redirect('/product');
-});
+// app.get("/", (req, res) => {
+//     // res.redirect('/product');
+// });
 
 app.use("/product", require("./routes/product"));
 
