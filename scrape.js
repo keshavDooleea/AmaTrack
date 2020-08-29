28462ab7332e8d1d76fa4a12f73f139c63a06df2
keshav <kdooleea@yahoo.ca>
2020-08-28 20:51:20 -0400

    idkk

--- scrape.js
+++ scrape.js
@@ -42,7 +42,7 @@ async function find(url) {
             waitUntil: ['load', 'networkidle2', 'domcontentloaded']
         })
         // await page.goto(url);
-        await page.waitFor(4000);
+        await page.waitFor(3000);
 
         // start scrapin here
 
@@ -62,19 +62,22 @@ async function find(url) {
             title = document.querySelector("#productTitle.a-size-large.product-title-word-break").innerText;
 
             // get stock amount
-            const stockHtmlTags = [document.querySelector(".a-size-medium.a-color-success"), document.querySelector(".a-size-medium.a-color-state")];
+            // const stockHtmlTags = [document.querySelector(".a-size-medium.a-color-success"), document.querySelector(".a-size-medium.a-color-state")];
+            const stockHtmlTags = [document.querySelector(".a-size-medium.a-color-success")];
             stockHtmlTags.forEach(tag => {
                 tag !== null || tag !== "" ? stockNb = tag.innerText : null;
             });
 
             // finding correct price
-            const priceHtmlTags = [document.querySelector("#price_inside_buybox.a-size-medium.a-color-price"), document.querySelector("#priceblock_ourprice.a-size-medium.a-color-price")];
+            // const priceHtmlTags = [document.querySelector("#price_inside_buybox.a-size-medium.a-color-price"), document.querySelector("#priceblock_ourprice.a-size-medium.a-color-price")];
+            const priceHtmlTags = [document.querySelector("#priceblock_ourprice.a-size-medium.a-color-price")];
             priceHtmlTags.forEach(tag => {
                 tag !== null || tag !== "" ? price = getNumber(tag.innerText) : null;
             });
 
             //getting shipping costs
-            const shippingHtmlTags = [document.querySelector("#ourprice_shippingmessage .a-color-secondary.a-size-base")];
+            // const shippingHtmlTags = [document.querySelector("#ourprice_shippingmessage .a-color-secondary.a-size-base")];
+            const shippingHtmlTags = [];
             shippingHtmlTags.forEach(tag => {
                 tag !== null || tag !== "" ? shipping = getNumber(tag.innerText) : shipping = 0;
             });
